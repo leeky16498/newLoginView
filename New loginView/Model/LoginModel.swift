d389cd02021b6125984a8325978909e664dd8688
Kyungyun Lee, IOS Dev <92150863+leeky16498@users.noreply.github.com>
2022-02-18 21:56:08 +0000

    Update LoginModel.swift

--- New loginView/Model/LoginModel.swift
+++ New loginView/Model/LoginModel.swift
@@ -6,28 +6,33 @@
 //
 
 import Foundation
-import Firebase
+import Firebase // firebase 임폴트해서 사용 준비
 import SwiftUI
 
 class LoginModel : ObservableObject {
     
-    let auth = Auth.auth()
+    let auth = Auth.auth() // Auth 기능 상수 선언
     
     @Published var singedIn : SignIn = .yet
     
     var isSignedIn : Bool {
-        return auth.currentUser == nil
+        return auth.currentUser == nil // 유저의 접속여부를 리턴해 줄 코드
     }
     
     func loginAccount(email : String, passWord : String) {
         
-        auth.signIn(withEmail: email, password: passWord) { [weak self] result, error in
+        auth.signIn(withEmail: email, password: passWord) { [weak self] result, error in // weak self로 선언한 이유는 메모리 누수를 방지하기 위해서이다.
         
             if result != nil {
-                self?.singedIn = .good
+                
+                DispatchQueue.main.async { 
+                    self?.singedIn = .good // 메인 스레드 작업 여건 보장을 위해서 디스패치큐를 걸어서 작업을 멀티 쓰레드로 옮겨준다.
+                }
             }
             if error != nil {
-                self?.singedIn = .fail
+                DispatchQueue.main.async { 
+                    self?.singedIn = .fail // 메인 스레드 작업 여건 보장을 위해서 디스패치큐를 걸어서 작업을 멀티 쓰레드로 옮겨준다.
+                }
             }
         }
     }
